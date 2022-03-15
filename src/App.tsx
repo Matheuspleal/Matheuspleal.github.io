26453de571aa1a0239eda3f5b697ac3daa88fbdf
Matheus Pimentel Leal <matheuspleal@pop-os.localdomain>
2022-03-15 00:08:39 -0300

    :rocket: adição dos botões sociais

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { BigText } from './assets/components/BigText';
 import Dev from './assets/img/developer.svg';
-import './assets/styles/App.css'
+import Github from './assets/img/github.svg';
+import Linkedin from './assets/img/linkedin.svg'
+import Mail from './assets/img/mail.svg'
+import './assets/styles/App.css';
 
 export function App() {
     return(
         <section className='content'>
             <BigText />
             <img src={ Dev } className='bigImg'/>
+            
+            {/* Criar componente só para os icons */}
+            <img src={ Github } className='icon'/>
+            <img src={ Linkedin } className='icon'/>
+            <img src={ Mail } className='icon'/>
         </section>
     )
 }
